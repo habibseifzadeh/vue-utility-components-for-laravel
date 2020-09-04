d02daef1e51783c52be481533999ada5e240157a
Habib Seifzadeh <habibseifzadeh@yahoo.com>
2020-09-04 21:19:38 +0430

    Update HabibMoneyLabelComponent.vue
    input value of moneylabel checked for invalid data

--- resources/js/components/HabibMoneyLabelComponent.vue
+++ resources/js/components/HabibMoneyLabelComponent.vue
@@ -10,6 +10,10 @@
         ],
         computed: {
             moneyValue() {
+                if(this.value === undefined || this.value === null) {
+                    return '';
+                }
+                let tempValue = this.value.toString();
                 let onlyNumbers = this.value.replace(/[^0-9]/g, '');
                 let onlyNumbersArr = onlyNumbers.split('');
                 for (let i = onlyNumbersArr.length - 3; i > 0; i -= 3) {
