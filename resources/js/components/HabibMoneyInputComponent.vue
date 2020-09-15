0e3b3b894ccc5883e3ebf95dc3899cfe28e617b3
Habib Seifzadeh <habibseifzadeh@yahoo.com>
2020-09-15 19:23:58 +0430

    Each MoneyInput component now has a unique id

--- resources/js/components/HabibMoneyInputComponent.vue
+++ resources/js/components/HabibMoneyInputComponent.vue
@@ -1,29 +1,27 @@
 <template>
-    <div>
         <input
-            id="money-input"
+            :id="'moneyInput' + id"
             type="text"
             inputmode="numeric"
             @keyup="valueChanged"
             :value="initialValue">
-    </div>
 </template>
 
 <script>
     export default {
         name: "HabibMoneyInputComponent",
         props: [
-            'initialValue'
+            'initialValue', 'id'
         ],
         methods: {
             valueChanged() {
-                let moneyInput = document.getElementById('money-input').value;
+                let moneyInput = document.getElementById('moneyInput' + this.id).value;
                 let onlyNumbers = moneyInput.replace(/[^0-9]/g, '');
                 let onlyNumbersArr = onlyNumbers.split('');
                 for(let i = onlyNumbersArr.length - 3; i > 0; i-=3) {
                     onlyNumbersArr.splice(i, 0, ',');
                 }
-                document.getElementById('money-input').value = onlyNumbersArr.join('');
+                document.getElementById('moneyInput' + this.id).value = onlyNumbersArr.join('');
                 this.$emit('value-changed', onlyNumbers);
             }
         },
